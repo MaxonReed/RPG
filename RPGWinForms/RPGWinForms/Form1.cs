35a0c61fad7405a164bab9001c0a7fd4c9209424
MaxonReed <49163206+MaxonReed@users.noreply.github.com>
2019-04-01 11:24:41 -0500

    toe

---
+++ RPGWinForms/RPGWinForms/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RPGv2;
+
+namespace RPGWinForms
+{
+    public partial class Form1 : Form
+    {
+        private static Form1 form = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            form = this;
+        }
+
+        public void LoadMap(Map m)
+        {
+            Bitmap bmp = new Bitmap(m.Img);
+            int w = bmp.Width;
+            int h = bmp.Height;
+            int winSizeH = form.Height;
+            int winSizeW = form.Width;
+            double ratio = 0;
+            ratio = winSizeH / h;
+            Bitmap resized = new Bitmap(bmp, new Size(Convert.ToInt32(w * ratio), Convert.ToInt32(h * ratio)));
+            pictureBox1.Image = resized;
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+        }
+    }
+}
